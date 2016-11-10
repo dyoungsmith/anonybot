3e7dacb7734b3c33de5e199219b5730efa00f0b5
Danielle YoungSmith <dyoungsmith@gmail.com>
2016-11-10 16:24:57 -0500

    webhooks added

--- env-dev.js
+++ env-dev.js
@@ -1,8 +1,10 @@
 const TOKEN = require('./token.js');
 let [ELIOT, DANI] = require('./fellows.js');
+const WEBHOOK = require('./webhook.js');
 
 module.exports = {
 	TOKEN,
 	ELIOT,
-	DANI
+	DANI,
+	WEBHOOK
 };
--- env-production.js
+++ env-production.js
@@ -1,5 +1,6 @@
 module.exports = {
 	TOKEN: process.env.SLACK_TOKEN,
 	ELIOT: process.env.ELIOT_SLACK_ID,
-	DANI: process.env.DANI_SLACK_ID
+	DANI: process.env.DANI_SLACK_ID,
+	WEBHOOK: process.env.WEBHOOK_TOKEN
 };
